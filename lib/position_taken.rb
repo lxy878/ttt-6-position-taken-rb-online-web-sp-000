d69f9212a0412925acfc5a61da4e5dcae28427e4
Xiaoyi Liu <lxy878@gmail.com>
2020-03-01 05:14:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,12 +2,6 @@
 
 def position_taken?(board, index)
 
-  # if board[index] == "" or board[index] == nil or board[index] == " "
-  #   return false
-  # else
-  #   return true
-  # end
-
   if board[index] == "X" or board[index] == "O"
     return true
   else
