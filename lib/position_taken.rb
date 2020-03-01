4c95f2edf2fd162d8105aa3b1747d8c7da031c53
Xiaoyi Liu <lxy878@gmail.com>
2020-03-01 05:06:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-
+  if board.length < 9
+    return false
+  end
   if board[index] == "X" or board[index] == "O"
     return false
   end
